fded6472dc8684e2ca5e957565bc34646b4db540
refute2change <madguyinthehouse@gmail.com>
2025-05-21 10:30:09 +0700

    initiate a file for fetching responses

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -6,6 +6,20 @@
 // {
 //     alert(12345);
 // }
+const responses = {"responses": {
+    "YES": "YESN'T",
+    "NO": "NON'T",
+    "MAYBE": "MAYBE NOT",
+    "OK": "OKAY",
+    "SURE": "SURELY",
+    "YESNO": "YESNOPE",
+    "YEP": "YEPER",
+    "NOPE": "NOPEY",
+    "YEAH": "YEAH-NOPE",
+    "NAH": "NAH-NOPE",
+    "YAY": "YAY-NAY",
+    "NAY": "NAY-YAY"}
+};
 
 function lmao()
 {
@@ -42,8 +56,10 @@ function addinputevent(x)
 function addresponseevent(x)
 {
     const element = document.getElementById("conversation");
+    const response = document.createElement("div"); // Create a new div element, will be used later on for actual multi-line response
     const respara = document.createElement("p"); // Create a new paragraph element
-    const resnode = document.createTextNode("You typed: " + x); // Create a text node with the input value
+    console.log(responses);
+    const resnode = document.createTextNode(`You typed: ${x}. Sadly whoever owns this has not guided me what to do, so I can't give you any explicit commands. Ask the author, bet he has everything under control.`); // Create a text node with the input value
     respara.classList.add("ourResponse"); // Set the class name for styling
     respara.appendChild(resnode); // Append the text node to the paragraph element
     element.appendChild(respara); // Append the paragraph element to the "conversation" element
@@ -53,9 +69,9 @@ function checkvalidity(event)
 {
     document.getElementById("answer").value = document.getElementById("answer").value.toUpperCase(); // To change the display string
     var x = document.getElementById("answer").value;
-    if (x.length >= 10)
+    if (x.length >= 256)
     {
-        document.getElementById("answer").value = x.substring(0, 10); // Limit input to 10 characters
+        document.getElementById("answer").value = x.substring(0, 256); // Limit input to 10 characters
         console.log("Input limited to 256 characters.");
     }
     event.preventDefault();
